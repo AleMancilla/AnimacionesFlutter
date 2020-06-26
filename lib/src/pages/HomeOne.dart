bdb46af06d601a76f13c21b23b2ebbd48a3a6ba5
Alejandro <alejandro.mancilla.umsa@gmail.com>
2020-06-25 22:03:21 -0400

    primer header

---
+++ lib/src/pages/HomeOne.dart
@@ -0,0 +1,12 @@
+import 'package:custom_painter/src/widgets/HeaderOne.dart';
+import 'package:flutter/material.dart';
+
+class HomeOne extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: HeaderOne(),
+    );
+  }
+}
---
+++ lib/src/widgets/HeaderOne.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HeaderOne extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300.0,
+      color: Colors.purpleAccent[900],
+    );
+  }
+}
