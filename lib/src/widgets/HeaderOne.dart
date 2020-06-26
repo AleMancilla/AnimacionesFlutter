ec3dacf44793e5dddd24564cf1b3d30cc609a357
Alejandro <alejandro.mancilla.umsa@gmail.com>
2020-06-25 23:04:05 -0400

    custom painter

--- lib/src/widgets/HeaderOne.dart
+++ lib/src/widgets/HeaderOne.dart
@@ -28,3 +28,49 @@ class HeaderRedondeado extends StatelessWidget {
     );
   }
 }
+
+class HeaderDiagonal extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      //color: Colors.purpleAccent,
+      child: CustomPaint(
+        painter: _HeaderDiagonalPainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderDiagonalPainter extends CustomPainter{
+  @override
+  void paint(Canvas canvas, Size size) {
+    // TODO: implement paint
+    final lapiz = Paint();
+    //propiedades
+    lapiz.color = Colors.purpleAccent;
+    lapiz.style = PaintingStyle.fill;//stroke 
+    lapiz.strokeWidth = 10;
+
+    final path = Path();
+
+    //dibujar con el path y el lapiz
+    path.moveTo(0, size.height*0.35);
+    path.lineTo(size.width, size.height *  0.30);
+    path.lineTo(size.width, size.height *  0);
+    path.lineTo(0,0);
+    path.lineTo(0, size.height*0.5);
+
+
+    canvas.drawPath(path, lapiz);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    // TODO: implement shouldRepaint
+    return true;
+  }
+
+}
