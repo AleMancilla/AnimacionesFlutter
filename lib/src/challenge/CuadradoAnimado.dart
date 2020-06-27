81467bebe22bb5e3a99f8d49ca041bbabb8175d2
Alejandro <alejandro.mancilla.umsa@gmail.com>
2020-06-27 00:53:01 -0400

    Tarea Challeng animate

--- lib/src/challenge/CuadradoAnimado.dart
+++ lib/src/challenge/CuadradoAnimado.dart
@@ -21,11 +21,26 @@ class CuadradoAnimado extends StatefulWidget {
   _CuadradoAnimadoState createState() => _CuadradoAnimadoState();
 }
 
-class _CuadradoAnimadoState extends State<CuadradoAnimado> {
+class _CuadradoAnimadoState extends State<CuadradoAnimado> with SingleTickerProviderStateMixin{
+
+  AnimationController controller;//linea de tiempo
+  Animation<double> moverDerecha;
+  Animation<double> moverIzquierda;
+  Animation<double> moverArriba;
+  Animation<double> moverAbajo;
 
   @override
   void initState() {
     // TODO: implement initState
+
+    controller = new AnimationController(vsync: this ,duration: Duration(milliseconds: 4000));
+    moverDerecha    = Tween(begin: 0.0, end: 200.0).animate(CurvedAnimation(parent: controller, curve:Interval(0.0, 0.25,curve: Curves.bounceInOut) ));
+    moverArriba     = Tween(begin: 0.0, end: 200.0).animate(CurvedAnimation(parent: controller, curve:Interval(0.25, 0.50,curve: Curves.bounceInOut) ));
+    moverIzquierda  = Tween(begin: 0.0, end: 200.0).animate(CurvedAnimation(parent: controller, curve:Interval(0.50, 0.75,curve: Curves.bounceInOut) ));
+    moverAbajo      = Tween(begin: 0.0, end: 200.0).animate(CurvedAnimation(parent: controller, curve:Interval(0.75, 1.0,curve: Curves.bounceInOut) ));
+    
+    
+
     super.initState();
   }
 
@@ -37,7 +52,23 @@ class _CuadradoAnimadoState extends State<CuadradoAnimado> {
 
   @override
   Widget build(BuildContext context) {
-    return  _Cuadrado();
+
+    controller.forward();
+
+    return  AnimatedBuilder(
+      animation: controller,
+      child: _Cuadrado(),
+      builder: (BuildContext context , Widget widgetCuadrado){
+        print("Derecha = ${moverDerecha.value}  -- Arriba = ${moverArriba.value}  --  Izquierda = ${moverIzquierda.value}  --  Abajo = ${moverAbajo.value}");
+        //print("Arriba = ${moverArriba.value}");
+        //print("Izquierda = ${moverIzquierda.value}");
+        //print("Abajo = ${moverAbajo.value}");
+        return Transform.translate(
+          offset: Offset(moverDerecha.value - moverIzquierda.value ,  (moverArriba.value - moverAbajo.value)*(-1)),
+          child: widgetCuadrado
+        );
+      },
+    );
   }
 }
 
