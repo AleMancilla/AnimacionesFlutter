c3dc45a00ff59887ed868f6d28357822e5c8987b
Alejandro <alejandro.mancilla.umsa@gmail.com>
2020-06-28 23:36:20 -0400

    poniendo dots

--- lib/src/pages/SlideShowPage.dart
+++ lib/src/pages/SlideShowPage.dart
@@ -7,7 +7,12 @@ class SlideShowPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body:  Container(
-        child: _Slides()
+        child: Column(
+          children: [
+            Expanded(child: _Slides()),
+            _Dots()
+          ],
+        )
         //WebsafeSvg.asset("assets/svgs/slide1.svg")
       )
         //SvgPicture.asset("assets/svgs/slide1.svg"),
@@ -15,6 +20,43 @@ class SlideShowPage extends StatelessWidget {
   }
 }
 
+
+class _Dots extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 70.0,
+      //color: Colors.red,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          _Dot(),
+          _Dot(),
+          _Dot(),
+          
+        ],
+      ),
+    );
+  }
+}
+
+class _Dot extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 12.0,
+      height: 12.0,
+      margin: EdgeInsets.symmetric(horizontal: 10.0),
+      decoration: BoxDecoration(
+        color:  Colors.grey,
+        shape: BoxShape.circle
+      ),
+    );
+  }
+}
 class _Slides extends StatelessWidget {
 
   @override
