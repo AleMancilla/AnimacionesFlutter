0e90e08e6fe8fd81da7abcc19f445f37cf71f277
Alejandro <alejandro.mancilla.umsa@gmail.com>
2020-06-28 22:46:08 -0400

    mostrando svg para todos

--- lib/src/pages/SlideShowPage.dart
+++ lib/src/pages/SlideShowPage.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:websafe_svg/websafe_svg.dart';
 
 class SlideShowPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Hola mari"),
-      ),
+      body:  Container(
+        child: WebsafeSvg.asset("assets/svgs/slide1.svg")
+      )
+        //SvgPicture.asset("assets/svgs/slide1.svg"),
     );
   }
 }
