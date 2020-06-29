8f1eeb724f29b86b011ee36bf53be33dc91bdccf
Alejandro <alejandro.mancilla.umsa@gmail.com>
2020-06-28 23:15:25 -0400

    svg pading sin molestias

--- lib/src/pages/SlideShowPage.dart
+++ lib/src/pages/SlideShowPage.dart
@@ -7,9 +7,43 @@ class SlideShowPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body:  Container(
-        child: WebsafeSvg.asset("assets/svgs/slide1.svg")
+        child: _Slides()
+        //WebsafeSvg.asset("assets/svgs/slide1.svg")
       )
         //SvgPicture.asset("assets/svgs/slide1.svg"),
     );
+  }
+}
+
+class _Slides extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child:PageView(
+        children: [
+          _Slide("assets/svgs/slide1.svg"),
+          _Slide("assets/svgs/slide2.svg"),
+          _Slide("assets/svgs/slide3.svg")
+        ],
+    )
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+
+  final String svg;
+  _Slide(this.svg);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      padding: EdgeInsets.all(30.0),
+      child: WebsafeSvg.asset(svg) ,
+    );
+    //WebsafeSvg.asset("assets/svgs/slide1.svg") ;
   }
 }
