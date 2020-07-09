a1b1f3204ecc0a6b7c09182ef8f3204f7dfa77aa
Alejandro <alejandro.mancilla.umsa@gmail.com>
2020-07-08 22:29:48 -0400

    mandando iconos al widget menu

--- lib/src/widgets/Pinteres_menu.dart
+++ lib/src/widgets/Pinteres_menu.dart
@@ -12,7 +12,7 @@ class PinteresButton{
 
 class PinteresMenu extends StatelessWidget {
 
-  final List items = [
+  final List<PinteresButton> items = [
     PinteresButton(icon: Icons.pie_chart,onPress: (){print("Icon pie char");}),
     PinteresButton(icon: Icons.search,onPress: (){print("Icon search");}),
     PinteresButton(icon: Icons.notifications,onPress: (){print("Icon notifications");}),
@@ -23,8 +23,51 @@ class PinteresMenu extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Container(
-        child: Text("Hola Mundo Desde El menu"),
+
+        child: _MenuItems(items),
+
+        width: 250.0,
+        height: 60.0,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(100.0),
+          boxShadow: <BoxShadow>[
+            BoxShadow(
+              color: Colors.black38,
+              //offset: Offset(10, 10),
+              blurRadius: 10.0,
+              spreadRadius: -5
+            )
+          ]
+        ),
       ),
+    );
+  }
+}
+
+class _MenuItems extends StatelessWidget {
+
+  final List<PinteresButton> menuItems;
+
+  _MenuItems(this.menuItems);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: List.generate(menuItems.length,(i)=> _PinteresMenuButton(i,menuItems[i])),
+    );
+  }
+}
+
+class _PinteresMenuButton extends StatelessWidget {
+  final int index;
+  final PinteresButton item;
+  _PinteresMenuButton(this.index,this.item);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Icon( item.icon),
     );
   }
 }
