5d991ad316890292e9c038c24360b6aaba3bdca1
Alejandro <alejandro.mancilla.umsa@gmail.com>
2020-07-10 21:35:19 -0400

    juntamos todo

--- lib/src/pages/Emergency_Page.dart
+++ lib/src/pages/Emergency_Page.dart
@@ -14,15 +14,49 @@ class EmergencyPage extends StatelessWidget {
       //  subtitulo: "Me encantas",
       //  colorTop: Colors.orange,
       //),
-      body: Center(
-        child: BotonGordo(
-          icono: FontAwesomeIcons.carCrash,
-          texto: "Hola Mari :* :*",
-          color1: Colors.blue,
-          color2: Colors.orange,
-          onPress: (){print("Hola Mari");},
-        )
-      ),
+      body: Stack(
+        children: [
+          
+          Container(
+            // padding: EdgeInsets.only(top: 50.0),
+            margin: EdgeInsets.only(top: 250.0),
+            child: ListView(
+              children: [
+                SizedBox(height: 50.0,),
+                _BotonGordoTemp(),
+                _BotonGordoTemp(),
+                _BotonGordoTemp(),
+                _BotonGordoTemp(),
+                _BotonGordoTemp(),
+                _BotonGordoTemp(),
+                _BotonGordoTemp(),
+              ],
+            ),
+          ),
+          IconHeaderWidget(
+            icono: FontAwesomeIcons.plus, 
+            titulo: "Asistencia Medica", 
+            subtitulo: "Has solicitado emergencia medica"
+          )
+        ],
+      )
+    );
+  }
+}
+
+class _BotonGordoTemp extends StatelessWidget {
+  const _BotonGordoTemp({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BotonGordo(
+      icono: FontAwesomeIcons.carCrash,
+      texto: "Hola Mari :* :*",
+      color1: Colors.blue,
+      color2: Colors.orange,
+      onPress: (){print("Hola Mari");},
     );
   }
 }
